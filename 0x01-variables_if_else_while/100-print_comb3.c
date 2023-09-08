d99d398e14ef048d0a78421189f982bb9d53a9fb
kawthar <kawtharikram2020@gmail.com>
2023-09-08 12:20:35 -0700

    hello

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+ */
+int main(void)
+{
+	int num1 = '0';
+	int num2 = '0';
+
+	while (num1 <= '9')
+	{
+		while (num2 <= '9')
+		{
+			if (!(num1 > num2 || num1 == num2))
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' && num2 == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num2 = '0';
+		num1++;
+	}
+	return (0);
+}
