93ce68fbe63c21efd36ccae9a814c8b52ce35f42
kawthar <kawtharikram2020@gmail.com>
2023-09-08 06:48:56 -0700

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Programming
+* Return: return value is 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
