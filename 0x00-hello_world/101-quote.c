1af0abe3a9dc0e024f78e83861996abe3330a9db
kawthar <kawtharikram2020@gmail.com>
2023-09-08 06:52:08 -0700

    hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming
+ * Return: return value is 1
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
