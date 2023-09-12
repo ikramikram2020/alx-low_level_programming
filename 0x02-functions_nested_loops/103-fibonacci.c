6b8ce31ce0f225d9c1077b988c80c709d262f9b4
kawthar <kawtharikram2020@gmail.com>
2023-09-12 13:08:58 -0700

    hello

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description - program that prints the first 50 Fibonacci numbers,
+*starting with 1 and 2, followed by a new line
+*Return: Always 0.
+*/
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
+	float tot_sum;
+
+	while (1)
+	{
+		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
+			break;
+		if ((fibsum % 2) == 0)
+			tot_sum += fibsum;
+
+		fib1 = fib2;
+		fib2 = fibsum;
+	}
+	printf("%.0f\n", tot_sum);
+
+	return (0);
+}}
