161b757d3e02202bb53352dd8e1b490403ddca3f
kawthar <kawtharikram2020@gmail.com>
2023-09-12 03:07:21 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,20 @@
 /**
  * main - Entry point
  *
- * Description: Prints _putchar using pitchar prototype
+ * Description: Prints _putchar, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+	char message[] = "_putchar\n";
+	int i;
 
-	for (ch = 0; ch < 8; ch++)
+	for (i = 0; message[i] != '\0'; i++)
 	{
-		_putchar(str[ch]);
-
-	_putchar('\n');
+		/* Use the _putchar function to print each character */
+		_putchar(message[i]);
 	}
+
 	return (0);
 }
-
