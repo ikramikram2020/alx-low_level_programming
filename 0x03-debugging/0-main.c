b9be2a80aa59a3f8fbd66c1c5aee261db55361c0
kawthar <kawtharikram2020@gmail.com>
2023-09-13 01:13:35 -0700

    hello

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,11 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;  // Change this line to test with 0
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
-}
+	return (0);
 }
