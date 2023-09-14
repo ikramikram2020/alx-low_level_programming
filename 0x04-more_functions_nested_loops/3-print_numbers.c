8afa13f4718bf95400de150c008b0e1bda20c695
kawthar <kawtharikram2020@gmail.com>
2023-09-14 07:26:04 -0700

    hello

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- *print_numbers - check the code.
- *descreption : prints the numbers, from 0 to 9, followed by a new line.
- *Return : nothing
+ * print_numbers - check the code.
+ *
+ * Return: Always .
  */
-void print_numbers(void);
+
+void print_numbers(void)
 {
 	int i;
 
