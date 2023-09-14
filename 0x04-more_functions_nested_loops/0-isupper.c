c70dcb38810cb840cd068440f8b2f97aac2d64c1
kawthar <kawtharikram2020@gmail.com>
2023-09-14 03:44:44 -0700

    hello

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,6 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);  // Return 1 if c is within the uppercase letter range (A-Z)
-    else
-        return (0);  // Return 0 if c is not within the uppercase letter range
+    return (c >= 'A' && c <= 'Z');
 }
 
