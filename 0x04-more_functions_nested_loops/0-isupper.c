c2b0fc26917472eb5a01838fca3bf522564b3e5d
kawthar <kawtharikram2020@gmail.com>
2023-09-14 04:47:50 -0700

    hello

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 /**
  * _isupper - Checks for uppercase character
- * @k: The character to be checked
+ * @c: The character to be checked
  *
- * Return: 1 if k is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int k)
+int _isupper(int c)
 {
-    if (k >= 'A' && k <= 'Z')
-        return (1);
-    else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-
