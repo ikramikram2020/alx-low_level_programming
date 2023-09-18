a7e5f9684921655cf36bc77304678c48c31b0457
kawthar <kawtharikram2020@gmail.com>
2023-09-18 07:04:24 -0700

    hello

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * swap_integers - Swaps the values of two integer variables.
+ * reset_to_98 - Resets the value of an integer pointer to 98.
  *
- * This function takes two integer pointers as its arguments and swaps the
- * values of the integers they point to. After the function call, the values
- * of the two integers will be exchanged.
+ * This function takes a pointer to an integer as its argument and sets the
+ * value of the integer to 98. It effectively changes the value pointed to by
+ * the pointer to 98.
  *
- * @a: A pointer to the first integer.
- * @b: A pointer to the second integer.
+ * @n: A pointer to an integer variable.
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
