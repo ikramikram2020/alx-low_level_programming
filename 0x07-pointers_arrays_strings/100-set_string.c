03cd418ac90f220b3494c38cc99845955ead866f
kawthar <kawtharikram2020@gmail.com>
2023-09-25 09:15:00 -0700

    hello

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,22 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_diagsums - fills memory with a constant byte.
- * @a: first bytes of the memory
- * @size: first bytes of the memory
- * Return: -
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which we print the sum of diagonals
+ * @size: size of the matrix
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int s1 = 0;
-	int s2 = 0;
+
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		s1 += *(a + i * size + i);
-		s2 += *(a + i * size + size - i - 1);
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
 
-	printf("%d, %d\n", s1, s2);
+	printf("%d, %d\n", sum, sum1);
 }
