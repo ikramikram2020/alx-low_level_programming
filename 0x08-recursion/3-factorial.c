bee0920a60ac04c5cd7cf1d630ea7e379468fd89
kawthar <kawtharikram2020@gmail.com>
2023-09-29 06:24:19 -0700

    hello

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,18 +9,23 @@
  */
 int factorial_helper(int n, int result)
 {
-    if (n == 0)
-        return result;
-
-    return factorial_helper(n - 1, n * result);
+	if (n == 0)
+	{
+		return (result);
+	}
+	return (factorial_helper(n - 1, n * result));
 }
-
+/**
+ * factorial - Calculates the factorial of a given number
+ * @n: The input number
+ *
+ * Return: The factorial of n, or -1 if n is negative
+ */
 int factorial(int n)
 {
-    
-    if (n < 0)
-        return -1;
-
-    return factorial_helper(n, 1);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (factorial_helper(n, 1));
 }
-
