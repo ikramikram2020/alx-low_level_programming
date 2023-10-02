ab7d391c66137c6259d52e3c9d49acec35a77f58
kawthar <kawtharikram2020@gmail.com>
2023-10-02 10:13:21 -0700

    hello

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments received.
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: if not receive 2 arg, rt error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+}
