c73c60ece2d7cc268774335c8eec2254c7ade2a0
kawthar <kawtharikram2020@gmail.com>
2023-10-02 10:04:58 -0700

    hello

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-print number of arguments passed
+ * @argc: num of commandline
+ * @argv: pointer to aarray of commandline
+ *
+ * 
+ * Return: o 
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
