092c9bc492ac4ce8b041313b8ccec8ca47cd5f1b
kawthar <kawtharikram2020@gmail.com>
2023-10-02 09:58:09 -0700

    hello

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: ?
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
