d61544c5ff15c13345c23233cdd69d86fb6169b4
kawthar <kawtharikram2020@gmail.com>
2023-10-11 12:56:45 -0700

    hello;

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,8 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef calc
+#define calc
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct op - Struct op
  *
@@ -11,10 +14,12 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,9 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * get_op_func - func that selects correct op_ to make operation
- * @s: Type char str
- * Return: pointer to op_ of a and b return c or null if not operation
+ * get_op_func - .
+ * @s: -
+ * Return: pointer.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -14,17 +13,16 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+		{NULL, NULL}};
+	int i = 0;
 
-
-	i = 0;
-	while (ops[i].op)
+	while (ops[i].op != NULL)
 	{
-		if (ops[i].op[0] == s[0] && s[1] == '\0')
+		if (s[0] == ops[i].op[0])
+		{
 			return (ops[i].f);
-		++i;
+		}
+		i++;
 	}
 	return (NULL);
 }
