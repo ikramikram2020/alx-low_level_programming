722606f4a5e3bac7ab088505eef0600fa766ce38
Khezour Kawthar Ikram <kawtharikram2020@gmail.com>
2023-10-19 12:29:36 -0700

    hello

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,13 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
- * @list: A pointer to the list where the new node will be added.
- * @data: The string data to be stored in the new node.
- *
- * This function creates a new node and appends it to the end of the given list.
- * It stores the provided string data and its length in the new node.
- *
+ * add_node_end - Appends a new node at the end of a list_t list.
  * @list: A pointer to the list where the new node will be added.
  * @data: The string data to be stored in the new node.
  *
@@ -15,32 +9,36 @@
  */
 list_t *add_node_end(list_t **list, const char *data)
 {
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *new_node, *temp;
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(data);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
+	new_node->len = 0;
+	while (data[new_node->len])
+		new_node->len++;
 
-	new_node->len = strlen(data);
 	new_node->next = NULL;
 
 	if (*list == NULL)
-		*list = new_node;
-	else
 	{
-		list_t *current = *list;
-
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new_node;
+		*list = new_node;
+		return (new_node);
 	}
-	return new_node;
+
+	temp = *list;
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_node;
+
+	return (new_node);
 }
 
