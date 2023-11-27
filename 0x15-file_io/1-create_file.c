953220ca901540316d390d91ea9375db69933e2a
kawthar <kawtharikram2020@gmail.com>
2023-11-27 08:22:47 -0800

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 
 /**
  * create_file - Creates a file with the given filename and writes the
@@ -12,19 +11,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, write_result, length;
+    int file_descriptor, write_result, length = 0;
 
     if (filename == NULL)
         return -1;
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     if (file_descriptor == -1)
         return -1;
 
     if (text_content != NULL)
     {
-        for (length = 0; text_content[length] != '\0'; length++)
-            continue;
+        while (text_content[length] != '\0')
+            length++;
 
         write_result = write(file_descriptor, text_content, length);
         if (write_result == -1)
